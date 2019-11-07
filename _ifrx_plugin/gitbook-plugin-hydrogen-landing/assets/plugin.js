f8c0355fbe9c0df152deb6fee49ed71c161d235e
jiamingli <jiamingli@ifanr.com>
2019-11-07 18:51:20 +0800

    完成首页样式改版，添加网站备案号

--- _ifrx_plugin/gitbook-plugin-hydrogen-landing/assets/plugin.js
+++ _ifrx_plugin/gitbook-plugin-hydrogen-landing/assets/plugin.js
@@ -29,6 +29,13 @@ function addCustomerBtn() {
   document.body.appendChild(node)
 }
 
+function addRecordNumber() {
+  let linkEle = document.querySelector('.gitbook-link')
+  linkEle.innerText = '粤 ICP 备 10211557 号-25'
+  linkEle.href = 'http://beian.miit.gov.cn'
+  linkEle.target = '_blank'
+}
+
 require(["gitbook", "jQuery"], function (gitbook, $) {
   gitbook.events.bind('start', function (e, config) {
     setTimeout(() => {
@@ -61,6 +68,13 @@ require(["gitbook", "jQuery"], function (gitbook, $) {
   })
 
   gitbook.events.bind('page.change', function () {
+    addRecordNumber()
+    const href_prod = 'https://doc.minapp.com/'
+    const href_dev = 'http://localhost:4000/'
+    if (location.href === href_prod || location.href === href_dev) {
+      const page = document.querySelector('.page-inner')
+      page.classList.add('index-page-inner')
+    }
     setTimeout(sidebarScrollIntoView, 300)
   })
 })
