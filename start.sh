3aa5fff86c17e02791f3bfbeff57520915a79ec5
Jiajun Yan <yanjiajun@ifanr.com>
2021-01-15 11:57:23 +0800

    修改 maxcdn 为 bootcdn

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
+# https://jira.corp.ifanr.com/browse/BAAS-6949
+# 替换 gitbook-plugin-simpletabs 插件中引用的 maxcdn 为国内可访问的地址
+sed -i 's,https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js,https://cdn.bootcdn.net/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js,g' ./node_modules/gitbook-plugin-simpletabs/index.js
+sed -i 's,https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css,https://cdn.bootcdn.net/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css,g' ./node_modules/gitbook-plugin-simpletabs/index.js
+
 gitbook serve --lrport 60032
